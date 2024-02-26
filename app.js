172cae1218d6944d8f11de116113d836181c6dae
Programmer NBA <137274609+programmer-nba@users.noreply.github.com>
2024-02-26 10:40:26 +0700

    up

--- app.js
+++ app.js
@@ -7,12 +7,12 @@ const cor = require('cors')
 require('dotenv').config()
 const mongoose = require('mongoose')
 
-process.env.TZ='UTC'
+process.env.TZ = 'UTC'
 var app = express();
 //เชื่ิอมdatabase
-const urldatabase =process.env.ATLAS_MONGODB
+const urldatabase = process.env.ATLAS_MONGODB
 mongoose.Promise = global.Promise
-mongoose.connect(urldatabase).then(()=>console.log("connect")).catch((err)=>console.error(err))
+mongoose.connect(urldatabase).then(() => console.log("connect")).catch((err) => console.error(err))
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -26,18 +26,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(cor())
 //router
 const prefix = '/v1/Backend-Partner'
-app.use(prefix+'/', require('./routes/index'));
+app.use(prefix + '/', require('./routes/index'));
 //ข้อมูล admin
-app.use(prefix+'/partner',require('./routes/partner'));
+app.use(prefix + '/partner', require('./routes/partner'));
 //ข้อมูลคู่ค้า
-app.use(prefix+'/dealer',require('./routes/dealer'));
-
-
+app.use(prefix + '/dealer', require('./routes/dealer'));
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*'); // หรือกำหนด origin ที่เฉพาะเจาะจง
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-  });
+  res.header('Access-Control-Allow-Origin', '*'); // หรือกำหนด origin ที่เฉพาะเจาะจง
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 const port = process.env.PORT || 9998;
 app.listen(port, console.log(`Listening on port ${port}`));
