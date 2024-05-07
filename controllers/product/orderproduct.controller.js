ea5b021d9dd57926feea4c778371c2e00b3f2e03
natthanon_sornsuwan <64368695+nonin22222@users.noreply.github.com>
2024-05-07 16:24:57 +0700

    7/5/67

--- controllers/product/orderproduct.controller.js
+++ controllers/product/orderproduct.controller.js
@@ -13,14 +13,16 @@ module.exports.create = async (req, res) => {
         //เช็คว่าของพอสั่งหรือเปล่า
         const product = req.body.product;
         product.forEach(async (element)=>{
-            const checkproduct = Product.findById(element.product_id);
+            const checkproduct = await Product.findById(element.product_id);
             if(checkproduct.product_stock <= element.product_qty){
                 return res.status(400).json({message:"สินค้า"+checkproduct.product_name+" มีไม่พอสั่ง", status: false});
             }
         })
         //ถ้าพอให้หักสต็อกสินค้าเลย
         product.forEach(async (element)=>{
-            const checkproduct = Product.findById(element.product_id);
+
+            const checkproduct = await Product.findById(element.product_id);
+          
             checkproduct.product_stock = checkproduct.product_stock - element.product_qty;
             await checkproduct.save();
         })
@@ -40,21 +42,98 @@ module.exports.create = async (req, res) => {
 };
 
 //แสดงออเดอร์ e-market ทั้งหมด
-
+module.exports.getall = async (req, res) => {
+    try{
+        const orderproduct = await Orderproduct.find();
+        return res.status(200).json({message:"แสดงออเดอร์สำเร็จ", status: true, data: orderproduct});
+    }catch(error){
+        return res.status(500).json({message:error.message, status: false});
+    }
+};
 
 //แสดงออเดอร์ e-market ตามไอดี
+module.exports.getbyid = async (req, res) => {
+    try{
+        const orderproduct = await Orderproduct.findById(req.params.id);
+        return res.status(200).json({message:"แสดงออเดอร์สำเร็จ", status: true, data: orderproduct});
+    }catch(error){
+        return res.status(500).json({message:error.message, status: false});
+    }
+};
 
 //แสดงออเดอร์ e-market ตามไอดี partner
+module.exports.getbyidpartner = async (req, res) => {
+    try{
+        const orderproduct = await Orderproduct.find({partner_id:req.params.id});
+        return res.status(200).json({message:"แสดงออเดอร์สำเร็จ", status: true, data: orderproduct});
+    }catch(error){
+        return res.status(500).json({message:error.message, status: false});
+    }
+};
 
 //แก้ไขออเดอร์ e-market ตามไอดี
+module.exports.update = async (req, res) => {
+    try{
+        const orderproduct = await Orderproduct.findById(req.params.id);
+        if(!orderproduct){
+            return res.status(400).json({message:"ไม่พบ order", status: false});
+        }
+       const update = await Orderproduct.findByIdAndUpdate(req.params.id, req.body,{new:true})
+        return res.status(200).json({message:"แก้ไขออเดอร์สำเร็จ", status: true, data: update});
+    }catch(error){
+        return res.status(500).json({message:error.message, status: false});
+    }
+};
 
 
 //ลบออเดอร์ e-market ตามไอดี
+module.exports.delete = async (req, res) => {
+    try{
+        const orderproduct = await Orderproduct.findById(req.params.id);
+        if(!orderproduct){
+            return res.status(400).json({message:"ไม่พบ order", status: false});
+        }
+        const del = await Orderproduct.findByIdAndDelete(req.params.id);
+        return res.status(200).json({message:"ลบออเดอร์สำเร็จ", status: true, data: del});
+    }catch(error){
+        return res.status(500).json({message:error.message, status: false});
+    }
+};
 
 // จัดส่งออเดอร์แล้ว
+module.exports.delivery = async (req, res) => {
+    try{
+        const orderproduct = await Orderproduct.findById(req.params.id);
+        if(!orderproduct){
+            return res.status(400).json({message:"ไม่พบ order", status: false});
+        }
+
+        orderproduct.statusdetail.push({status:"จัดส่งแล้ว",date:Date.now()});
+        const data ={
+            statusdetail:orderproduct.statusdetail,
+            tracking: req.body.tracking  
+        }
+        const update = await Orderproduct.findByIdAndUpdate(req.params.id, data,{new:true})
+        return res.status(200).json({message:"จัดส่งออเดอร์สำเร็จ", status: true, data: update});
+    }catch(error){
+        return res.status(500).json({message:error.message, status: false});
+    }
+};
 
 // ยกเลิกออเดอร์
-
+module.exports.cancel = async (req, res) => {
+    try{
+        const orderproduct = await Orderproduct.findById(req.params.id);
+        if(!orderproduct){
+            return res.status(400).json({message:"ไม่พบ order", status: false});
+        }
+        orderproduct.statusdetail.push({status:"ยกเลิกออเดอร์",date:Date.now()});
+        const update = await Orderproduct.findByIdAndUpdate(req.params.id, orderproduct,{new:true})
+        return res.status(200).json({message:"ยกเลิกออเดอร์สำเร็จ", status: true, data: update});
+    }catch(error){
+        return res.status(500).json({message:error.message, status: false});
+    }
+};
 
 
 //ฟังก์ชั่น สร้างเลขอ้างอิง order
@@ -86,4 +165,4 @@ async function runreforder() {
     return ref;
 
     
-}
+}   
--- routes/product/orderproduct.js
+++ routes/product/orderproduct.js
@@ -3,7 +3,28 @@ const router = express.Router();
 const orderproduct = require("../../controllers/product/orderproduct.controller")
 const auth = require('../../authentication/Auth')
 
+//สร้างออเดอร์ e-market
 router.post('/',orderproduct.create);
 
+//แสดงออเดอร์ e-market ทั้งหมด
+router.get('/',orderproduct.getall);
+
+//แสดงออเดอร์ e-market ตามไอดี
+router.get('/byid/:id',orderproduct.getbyid);
+
+//แสดงออเดอร์ e-market ตามไอดี partner
+router.get('/bypartner/:id',orderproduct.getbyidpartner);
+
+//แก้ไขออเดอร์ e-market ตามไอดี
+router.put('/:id',orderproduct.update);
+
+//ลบออเดอร์ e-market ตามไอดี
+router.delete('/:id',orderproduct.delete);
+
+// จัดส่งออเดอร์แล้ว
+router.put('/delivery/:id',orderproduct.delivery);
+
+// ยกเลิกออเดอร์
+router.put('/cancel/:id',orderproduct.cancel);
 
 module.exports = router;
